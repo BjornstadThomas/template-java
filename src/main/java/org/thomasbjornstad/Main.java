c94c6056c58f3bcac307ed15ebb1477af6361fc4
Thomas <thomasfb@stud.ntnu.no>
2023-07-27 14:52:57 +0200

    Edited Main.java to follow checkstyle

--- src/main/java/org/thomasbjornstad/Main.java
+++ src/main/java/org/thomasbjornstad/Main.java
@@ -1,6 +1,22 @@
 package org.thomasbjornstad;
 
-public class Main {
+/**
+ * The Main class of the application.
+ */
+public final class Main {
+
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private Main() {
+        // This constructor is intentionally empty.
+    }
+
+    /**
+     * The main method of the application.
+     *
+     * @param args The command-line arguments.
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
